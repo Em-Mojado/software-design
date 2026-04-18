8a7d2bfabc4006fec5b7cc4f65a92a8d1fcc64b4
Em-Mojado <e.mojado.554395@umindanao.edu.ph>
2026-04-18 19:08:55 +0800

    week 4

--- lab4/Form1.cs
+++ lab4/Form1.cs
@@ -1,3 +1,5 @@
+using System.Net;
+
 namespace lab4
 {
 	public partial class Form1 : Form
@@ -6,5 +8,41 @@ namespace lab4
 		{
 			InitializeComponent();
 		}
-	}
+
+		public class Book
+		{
+			public string Title { get; set; }
+			public string Author { get; set; }
+			
+			public virtual string GetInfo()
+			{
+                return $"Title: {Title}, Author: {Author}";
+            }
+        }
+		public class Magazine : Book
+		{
+			public int IssueNumber { get; set; }
+
+			public override string GetInfo()
+			{
+				return $"Title: {Title}, Author: {Author}, Issue Number: {IssueNumber}";
+            }
+
+        }
+        public void TestBookCreation()
+        {
+            Book book = new Book { Title = "C# Programming", Author = "Jhon Smith" };
+            Assert.AreEqual("C# Programming", book.Title);
+			Assert.AreEqual("Jhon Smith", book.Author);
+        }
+
+        public void TextMagazineCreation()
+        {
+            Magazine magazine = new Magazine { Title = "Tech Today", IssueNumber = 42 };
+            Assert.AreEqual("Tech Today", magazine.Title);
+            Assert.AreEqual(42, magazine.IssueNumber);
+        }
+
+
+    }
 }
